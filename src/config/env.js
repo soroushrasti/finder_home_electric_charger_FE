d8bc648acda6a50e6c67c84ae9efecf764c1f2f7
Soroush Rasti <rasti.soroush@gmail.com>
2025-08-07 01:55:25 +0200

    fix the bug for editing car

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 // src/config/env.js
 export default {
     dev: {
-        apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0LXVzZXIiLCJleHAiOjE3ODYwNDg3MTl9.DtgX2Dq2qrUbj2rWr32cOv6F0JP0J0LmVsJJg0P5jUY'
+        apiToken: ''
     },
     prod: {
-        apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0LXVzZXIiLCJleHAiOjE3ODYwNDg3MTl9.DtgX2Dq2qrUbj2rWr32cOv6F0JP0J0LmVsJJg0P5jUY'
+        apiToken: ''
     }
 };
